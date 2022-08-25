2a685faf57ff0a203bb9bf84f7337c1dbf0f50b6
Jason Cheatham <jason@jasoncheatham.com>
2022-08-24 20:10:50 -0400

    Conditionally load signal server in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,36 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { createSignalServer } from './scripts/serverUtil.js';
 import { readFileSync } from 'fs';
 
-/** @type {import('vite').Plugin} */
-const signalServer = {
-	name: 'signalServer',
-	configureServer: async function (server) {
-		createSignalServer(server.httpServer, '/ss');
-	}
-};
-
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit(), signalServer]
-};
-
-const { SSL_CRT, SSL_KEY } = process.env;
-if (SSL_CRT && SSL_KEY) {
-	config.server = {
-		https: {
-			key: readFileSync(SSL_KEY),
-			cert: readFileSync(SSL_CRT)
-		}
+/** @type {import('vite').UserConfigFn} */
+export default async function defineConfig({ command }) {
+	/** @type {import('vite').UserConfig} */
+	const config = {
+		plugins: [sveltekit()]
 	};
-}
 
-export default config;
+	if (command === 'serve') {
+		const { createSignalServer } = await import('./scripts/serverUtil.js');
+
+		/** @type {import('vite').Plugin} */
+		const signalServer = {
+			name: 'signalServer',
+			configureServer: async function (server) {
+				createSignalServer(server.httpServer, '/ss');
+			}
+		};
+
+		config.plugins.push(signalServer);
+	}
+
+	const { SSL_CRT, SSL_KEY } = process.env;
+	if (SSL_CRT && SSL_KEY) {
+		config.server = {
+			https: {
+				key: readFileSync(SSL_KEY),
+				cert: readFileSync(SSL_CRT)
+			}
+		};
+	}
+
+	return config;
+}
